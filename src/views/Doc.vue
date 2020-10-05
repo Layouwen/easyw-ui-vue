d13a00e7f04c7c18dc5579c8822d35b9281be722
Layouwen <Layouwen@gmail.com>
2020-10-05 20:24:47 +0800

    modify the style of Switch, Topnav and Doc pages

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav/>
+  <div class="layout">
+    <Topnav class="nav"/>
     <div class="content">
       <aside v-if="asideVisible">
         <h2>组件列表</h2>
@@ -42,9 +42,47 @@
 </script>
 
 <style lang="scss" scoped>
+  .layout {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    
+    > .nav {
+      flex-shrink: 0;
+    }
+    
+    > .content {
+      flex-grow: 1;
+      padding-top: 60px;
+      padding-left: 156px;
+      @media (max-width: 500px) {
+        padding-left: 0;
+      }
+    }
+  }
+  
+  .content {
+    display: flex;
+    
+    > aside {
+      flex-shrink: 0;
+    }
+    
+    > main {
+      flex-grow: 1;
+      padding: 16px;
+      background: lightgreen;
+    }
+  }
+  
   aside {
     width: 150px;
+    padding: 70px 16px 16px 16px;
     background: lightblue;
+    position: fixed;
+    top: 0;
+    left: 0;
+    height: 100%;
     
     > h2 {
       margin-bottom: 4px;
@@ -57,12 +95,7 @@
     }
   }
   
-  @media (max-width: 500px) {
-    aside {
-      padding: 70px 16px 16px 16px;
-      position: fixed;
-      top: 0;
-      left: 0;
-    }
+  main {
+    overflow: auto;
   }
 </style>
