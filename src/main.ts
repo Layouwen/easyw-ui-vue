ee09be9c2328e466b295314788fb104026987f96
Layouwen <Layouwen@gmail.com>
2020-10-05 11:44:06 +0800

    更新Home.vue的样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import {createApp} from 'vue'; // 创建vue实例的函数
-import App from './App.vue';
 import {createWebHashHistory, createRouter} from 'vue-router';
-import './index.css';
+import App from './App.vue';
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
+import './index.scss';
 
 const history = createWebHashHistory();
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
     <div class="topnav">
-      <div class="log"></div>
-      <div class="menu"></div>
+      <div class="logo">LOGO</div>
+      <ul class="menu">
+        <li>菜单1</li>
+        <li>菜单2</li>
+      </ul>
     </div>
     <div class="banner">
       <h1>Easy-UI</h1>
@@ -22,5 +25,24 @@
 </script>
 
 <style lang="scss" scoped>
-
+  .topnav {
+    background: pink;
+    display: flex;
+    padding: 16px;
+    
+    > .logo {
+      max-width: 6em;
+      margin-right: auto;
+    }
+    
+    > .menu {
+      display: flex;
+      white-space: nowrap;
+      flex-wrap: nowrap;
+      
+      > li {
+        margin: 0 1em;
+      }
+    }
+  }
 </style>
