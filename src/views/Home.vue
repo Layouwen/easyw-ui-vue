015bf47f304c8ba3c9ad0c6656ea90f706aa8540
Layouwen <Layouwen@gmail.com>
2020-10-05 11:23:54 +0800

    finsh Doc and Home Html

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="log"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>Easy-UI</h1>
+      <h2>一个很厉害的 UI 框架</h2>
+      <p class="actions">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Home",
+  }
+</script>
+
+<style scoped>
+
+</style>
