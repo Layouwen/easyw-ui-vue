684d0eb00dbdff1b721e808cb7807a56210d4cd9
Layouwen <Layouwen@gmail.com>
2020-10-05 21:17:20 +0800

    use props and context to complete the external parameter control state

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <button :class="{checked}" @click="toggle"><span></span></button>
+    <button :class="{checked: value}" @click="toggle"><span></span></button>
   </div>
 </template>
 
 <script lang='ts'>
-  import {ref} from 'vue';
-
   export default {
     name: 'Switch',
-    setup() {
-      const checked = ref(false);
+    props: {
+      value: Boolean,
+    },
+    setup(props, context) {
       const toggle = () => {
-        checked.value = !checked.value;
+        context.emit('input', !props.value);
       };
-      return {checked, toggle};
+      return {toggle};
     },
   };
 </script>
@@ -22,6 +22,10 @@
 <style lang="scss" scoped>
   $h: 22px;
   $h2: $h - 4px;
+  button:focus {
+    outline: none;
+  }
+  
   button {
     height: $h;
     width: $h*2;
@@ -39,9 +43,14 @@
     width: $h2;
     background: white;
     border-radius: $h2 / 2;
+    transition: left 250ms;
   }
   
-  button.checked span {
-    left: calc(100% - #{$h2} - 2px);
+  button.checked {
+    background: blue;
+    
+    > span {
+      left: calc(100% - #{$h2} - 2px);
+    }
   }
 </style>
