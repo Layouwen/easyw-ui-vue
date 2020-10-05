c6dfa6a0942ba0e9f9ed598a23307f6d8a72840f
Layouwen <Layouwen@gmail.com>
2020-10-05 18:02:24 +0800

    add toggleAside function

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="topnav">
-    <div class="logo" @click="toggleAside">LOGO</div>
+    <div class="logo">LOGO</div>
     <ul class="menu">
       <li>菜单1</li>
       <li>菜单2</li>
     </ul>
-    <span class="toggleAside"></span>
+    <span class="toggleAside" @click="toggleAside"></span>
   </div>
 </template>
 
@@ -50,7 +50,7 @@
     }
     
     > .toggleAside {
-      display: inline-block;
+      display: none;
       width: 24px;
       height: 24px;
       background: red;
@@ -66,6 +66,9 @@
       }
       > .logo {
         margin: 0 auto;
+      }
+      > .toggleAside {
+        display: inline-block
       }
     }
   }
