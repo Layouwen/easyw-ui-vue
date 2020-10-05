1d6a83cfd6976390a79844874d08d502535c6a28
Layouwen <Layouwen@gmail.com>
2020-10-05 12:00:50 +0800

    package Topnav components

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,9 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo"></div>
-      <div class="menu"></div>
-    </div>
+    <Topnav/>
     <div class="content">
       <aside>边栏</aside>
       <main>主内容</main>
@@ -11,10 +8,14 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+
+  import Topnav from '../components/Topnav.vue';
+
   export default {
-    name: "Doc.vue",
-  }
+    name: 'Doc',
+    components: {Topnav},
+  };
 </script>
 
 <style lang="scss" scoped>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <div class="logo">LOGO</div>
-      <ul class="menu">
-        <li>菜单1</li>
-        <li>菜单2</li>
-      </ul>
-    </div>
+    <Topnav/>
     <div class="banner">
       <h1>Easy-UI</h1>
       <h2>一个很厉害的 UI 框架</h2>
@@ -18,31 +12,13 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+  import Topnav from '../components/Topnav.vue'
   export default {
     name: "Home",
+    components: {Topnav}
   }
 </script>
 
 <style lang="scss" scoped>
-  .topnav {
-    background: pink;
-    display: flex;
-    padding: 16px;
-    
-    > .logo {
-      max-width: 6em;
-      margin-right: auto;
-    }
-    
-    > .menu {
-      display: flex;
-      white-space: nowrap;
-      flex-wrap: nowrap;
-      
-      > li {
-        margin: 0 1em;
-      }
-    }
-  }
 </style>
