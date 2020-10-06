54e644ca01329bdf69a982cf39b327201bcabeb2
liangyouwen <layouwen@gmail.com>
2020-10-07 04:02:17 +0800

    add Button theme style

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -56,5 +56,25 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  
+  &.easyw-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    
+    &:hover, &:focus {
+      color: lighten($blue, 10%);
+    }
+  }
+  
+  &.easyw-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    
+    &:hover, &:focus {
+      background: darken(white, 5%);;
+    }
+  }
 }
 </style>
