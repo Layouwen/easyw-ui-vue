6114088b77b09fd9771cc70fb34482503c8b72be
liangyouwen <layouwen@gmail.com>
2020-10-07 01:17:05 +0800

    create Button component

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,33 +1,20 @@
 <template>
   <div>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
-    <p>buttonDemo</p>
+    <div>Button 实例</div>
+    <h1>示例1</h1>
+    <div>
+      <Button>按钮</Button>
+    </div>
   </div>
 </template>
 
 <script lang='ts'>
-  export default {
-    name: 'ButtonDemo',
-  };
+import Button from '../lib/Button.vue';
+
+export default {
+  name: 'ButtonDemo',
+  components: {Button},
+};
 </script>
 
 <style lang='scss' scoped>
