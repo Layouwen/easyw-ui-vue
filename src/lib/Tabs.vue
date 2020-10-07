5156a8a899cbe47e3309586621c544ef00381a68
liangyouwen <layouwen@gmail.com>
2020-10-07 14:19:09 +0800

    add css styles for Tab and Tabs

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,12 @@
 <template>
-  <div v-for="(t,index) in titles" :key="index">{{t}} 1312312</div>
-  <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+  <div class="easyw-tabs">
+    <div class="easyw-tabs-nav">
+      <div class="easyw-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="easyw-tabs-content">
+      <component class="easyw-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -20,7 +26,6 @@ export default {
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
-    console.log(titles);
     return {
       defaults,
       titles,
@@ -30,5 +35,32 @@ export default {
 </script>
 
 <style lang="scss">
-
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.easyw-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      
+      &:first-child {
+        margin-left: 0;
+      }
+      
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
