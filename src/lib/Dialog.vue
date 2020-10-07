75673ccda68a6c24325e19360beed594dab72f92
liangyouwen <layouwen@gmail.com>
2020-10-07 12:54:57 +0800

    use Teleport to place components at the bottom

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,21 +1,23 @@
 <template>
   <template v-if="visible">
-    <div class="easyw-dialog-overlay" @click="onClickOverlay"></div>
-    <div class="easyw-dialog-wrapper">
-      <div class="easyw-dialog">
-        <header>
-          <slot name="title"/>
-          <span class="easyw-dialog-close" @click="close"></span>
-        </header>
-        <main>
-          <slot name="content"/>
-        </main>
-        <footer>
-          <Button level="main" @click="ok">OK</Button>
-          <Button @click="cancel">Cancel</Button>
-        </footer>
+    <Teleport to="body">
+      <div class="easyw-dialog-overlay" @click="onClickOverlay"></div>
+      <div class="easyw-dialog-wrapper">
+        <div class="easyw-dialog">
+          <header>
+            <slot name="title"/>
+            <span class="easyw-dialog-close" @click="close"></span>
+          </header>
+          <main>
+            <slot name="content"/>
+          </main>
+          <footer>
+            <Button level="main" @click="ok">OK</Button>
+            <Button @click="cancel">Cancel</Button>
+          </footer>
+        </div>
       </div>
-    </div>
+    </Teleport>
   </template>
 </template>
 
