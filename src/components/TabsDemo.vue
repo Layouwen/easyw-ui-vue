229e5c23ba850effe7193ff5937acdf287c0b7f5
liangyouwen <layouwen@gmail.com>
2020-10-07 23:30:55 +0800

    set default value selected

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,19 +1,24 @@
 <template>
   <div>Tabs 示例</div>
   <h1>示例1</h1>
-  <Tabs>
+  <Tabs v-model:selected='x'>
     <Tab title="导航1">内容1</Tab>
-    <Tab title="导航2">内容1</Tab>
+    <Tab title="导航2">内容2</Tab>
   </Tabs>
 </template>
 
 <script lang='ts'>
+import {ref} from 'vue';
 import Tab from '../lib/Tab.vue';
 import Tabs from '../lib/Tabs.vue';
 
 export default {
   name: 'TabsDemo',
   components: {Tab, Tabs},
+  setup() {
+    const x = ref('导航1');
+    return {x};
+  },
 };
 </script>
 
