362866332b83478b7584bad265072c84838b9a08
liangyouwen <layouwen@gmail.com>
2020-10-07 23:37:40 +0800

    use props to control the selected value to display content

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,19 +1,28 @@
 <template>
   <div class="easyw-tabs">
     <div class="easyw-tabs-nav">
-      <div class="easyw-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="easyw-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t ===
+      selected}" :key="index">{{t}}
+      </div>
     </div>
     <div class="easyw-tabs-content">
-      <component class="easyw-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index"/>
+      <component class="easyw-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults"
+                 :is="c"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import {computed} from 'vue';
 import Tab from './Tab.vue';
 
 export default {
   name: 'Tabs',
+  props: {
+    selected: {
+      type: String,
+    },
+  },
   setup(props, context) {
     const defaults = context.slots.default();
     // 判断标签是否使用Tab
@@ -22,13 +31,23 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab');
       }
     });
+    const current = computed(() => {
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected;
+      })[0];
+    });
     // 提取里面的title中出来
     const titles = defaults.map((tag) => {
       return tag.props.title;
     });
+    const select = (title: String) => {
+      context.emit('update:selected', title);
+    };
     return {
       defaults,
       titles,
+      current,
+      select,
     };
   },
 };
@@ -61,6 +80,14 @@ $border-color: #d9d9d9;
   
   &-content {
     padding: 8px 0;
+    
+    &-item {
+      display: none;
+      
+      &.selected {
+        display: block;
+      }
+    }
   }
 }
 </style>
