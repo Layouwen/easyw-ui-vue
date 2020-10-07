d30abe14d7fff42ad289f996643b647f0622bc3f
liangyouwen <layouwen@gmail.com>
2020-10-07 11:13:50 +0800

    add level style

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,12 @@
 <template>
-  <!-- :class="{[`theme-${theme}`]: theme}" -->
-  <button class="easyw-button"
-          :class="classes">
+  <button class="easyw-button" :class="classes">
     <slot/>
   </button>
 </template>
-
-<script lang='ts'>
+<script lang="ts">
 import {computed} from 'vue';
 
 export default {
-  name: 'Button',
   props: {
     theme: {
       type: String,
@@ -20,26 +16,31 @@ export default {
       type: String,
       default: 'normal',
     },
+    level: {
+      type: String,
+      default: 'normal',
+    },
   },
   setup(props) {
-    const {theme, size} = props;
+    const {theme, size, level} = props;
     const classes = computed(() => {
       return {
         [`easyw-theme-${theme}`]: theme,
         [`easyw-size-${size}`]: size,
+        [`easyw-level-${level}`]: level,
       };
     });
     return {classes};
   },
 };
 </script>
-
-<style lang='scss'>
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .easyw-button {
   box-sizing: border-box;
   height: $h;
@@ -54,6 +55,7 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
+  transition: background 250ms;
   
   & + & {
     margin-left: 8px;
@@ -78,7 +80,8 @@ $radius: 4px;
     box-shadow: none;
     color: $blue;
     
-    &:hover, &:focus {
+    &:hover,
+    &:focus {
       color: lighten($blue, 10%);
     }
   }
@@ -88,21 +91,79 @@ $radius: 4px;
     box-shadow: none;
     color: inherit;
     
-    &:hover, &:focus {
-      background: darken(white, 5%);;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
     }
   }
   
   &.easyw-size-big {
     font-size: 24px;
     height: 48px;
-    padding: 0 16px
+    padding: 0 16px;
   }
   
   &.easyw-size-small {
     font-size: 12px;
     height: 20px;
     padding: 0 4px;
+  }
+  
+  &.easyw-theme-button {
+    &.easyw-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
+    }
+    
+    &.easyw-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+  
+  &.easyw-theme-link {
+    &.easyw-level-danger {
+      color: $red;
+      
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
+  }
+  
+  &.easyw-theme-text {
+    &.easyw-level-main {
+      color: $blue;
+      
+      &:hover,
+      &:focus {
+        color: darken($blue, 10%);
+      }
+    }
+    
+    &.easyw-level-danger {
+      color: $red;
+      
+      &:hover,
+      &:focus {
+        color: darken($red, 10%);
+      }
+    }
   }
 }
 </style>
