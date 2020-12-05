bfd0116b2e73862a155d6dd84231e0f25d29d147
liangyouwen <layouwen@gmail.com>
2020-12-05 22:19:53 +0800

    尝试 attrs

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,11 +1,24 @@
 <template>
-ButtonDemo
+  <div>Button 示例</div>
+  <h1>示例1</h1>
+  <div>
+    <Button @click="test">你好</Button>
+  </div>
 </template>
 
 <script lang='ts'>
+import Button from '../lib/Button.vue';
+
 export default {
-name: "ButtonDemo"
-}
+  name: 'ButtonDemo',
+  components: {Button},
+  setup() {
+    const test = () => {
+      console.log('你好');
+    };
+    return {test};
+  },
+};
 </script>
 
 <style scoped>
---
+++ src/lib/Button.vue
@@ -0,0 +1,25 @@
+<template>
+  <div :size="size">
+    <!-- 通过绑定$attrs，绑定外部的属性 -->
+    <!-- <button v-bind="$attrs"> -->
+    <button v-bind="rest">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'Button.vue',
+  // 取消默认继承外部属性
+  inheritAttrs: false,
+  setup(props, context) {
+    const {size, ...rest} = context.attrs;
+    return {size, rest};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
