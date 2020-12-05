e878eecb8ae891cf3652c34fe598fbdaf0286baa
liangyouwen <layouwen@gmail.com>
2020-12-05 19:59:04 +0800

    添加doc默认路由

---
+++ src/components/DocDemo.vue
@@ -0,0 +1,13 @@
+<template>
+  Doc默认页面
+</template>
+
+<script lang='ts'>
+export default {
+  name: 'DocDemo',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router.ts
+++ src/router.ts
@@ -1,9 +1,10 @@
 import Doc from './View/Doc.vue';
 import Home from './View/Home.vue';
+import DocDemo from './components/DocDemo.vue';
 import SwitchDemo from './components/SwitchDemo.vue';
-import ButtonDemo from './components/ButtonDemo.vue';
-import DialogDemo from './components/DialogDemo.vue';
 import TabsDemo from './components/TabsDemo.vue';
+import DialogDemo from './components/DialogDemo.vue';
+import ButtonDemo from './components/ButtonDemo.vue';
 import './index.scss';
 import { createWebHashHistory, createRouter } from 'vue-router';
 
@@ -16,6 +17,7 @@ export const router = createRouter({
     {path: '/', component: Home},
     {
       path: '/doc', component: Doc, children: [
+        {path: '', component: DocDemo},
         {path: '/doc/switch', component: SwitchDemo},
         {path: '/doc/button', component: ButtonDemo},
         {path: '/doc/dialog', component: DialogDemo},
