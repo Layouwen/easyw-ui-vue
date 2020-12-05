ee55dd11e28e827b13bfe502e81de3aea2db78d2
liangyouwen <layouwen@gmail.com>
2020-12-05 21:42:06 +0800

    优化 Switch 的样式结构

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -21,43 +21,27 @@ export default {
 $h: 22px;
 $h2: $h - 4px;
 button {
-  position: relative;
-  height: $h;
-  width: $h*2;
-  border: none;
-  border-radius: $h / 2;
-  background: #bfbfbf;
+  position: relative; height: $h; width: $h*2;
+  border: none; border-radius: $h / 2; background: #bfbfbf;
 
-  span {
-    position: absolute;
-    top: 2px;
-    left: 2px;
-    width: $h2;
-    height: $h2;
-    border-radius: $h2 / 2;
-    background: white;
-    transition: all 250ms;
+  > span {
+    position: absolute; top: 2px; left: 2px;
+    width: $h2; height: $h2; border-radius: $h2 / 2;
+    background: white; transition: all 250ms;
   }
 
   &:active {
-    > span {
-      width: $h2 + 4px;
-    }
+    > span { width: $h2 + 4px; }
   }
 
   &.checked {
     background: #1890ff;
 
     &:active {
-      > span {
-        width: $h2 + 4px;
-        margin-left: -4px;
-      }
+      > span { width: $h2 + 4px; margin-left: -4px; }
     }
 
-    > span {
-      left: calc(100% - #{$h2} - 2px);
-    }
+    > span { left: calc(100% - #{$h2} - 2px); }
   }
 }
 </style>
