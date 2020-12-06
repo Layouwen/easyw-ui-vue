7c399afd366650fdf62886f02143a5551ada2f2b
liangyouwen <layouwen@gmail.com>
2020-12-06 12:40:36 +0800

    将 Tabs 和 Tab 渲染到页面

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     Tabs 组件
-    <component :is="defaults[0]" />
-    <component :is="defaults[1]" />
+    <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
+    <component v-for="(c, index) in defaults" :key="index" :is="c" />
   </div>
 </template>
 
@@ -12,13 +12,19 @@ import Tab from './Tab.vue';
 export default {
   name: 'Tabs',
   setup(props, context) {
+    // 获取元素数组
     const defaults = context.slots.default();
+    // 遍历元素数组，判断类型是否为Tab
     defaults.forEach((tag, index) => {
       if (tag.type !== Tab) {
         throw new Error('第' + index + '个' + tag.type + '元素不符合标准');
       }
     });
-    return {defaults};
+    // 将标签中的title值取出来
+    const titles = defaults.map((tag) => {
+      return tag.props.title;
+    });
+    return {defaults, titles};
   },
 };
 </script>
