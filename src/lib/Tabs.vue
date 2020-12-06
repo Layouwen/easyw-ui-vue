64269c45a95708ff32a4d81f700c5f19b6f0881a
liangyouwen <layouwen@gmail.com>
2020-12-06 12:49:03 +0800

    基本完成 Tabs 和 Tab 的样式

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    Tabs 组件
-    <div v-for="(t,index) in titles" :key="index">{{ t }}</div>
-    <component v-for="(c, index) in defaults" :key="index" :is="c" />
+  <div class="abc-tabs">
+    <div class="abc-tabs-nav">
+      <div class="abc-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{ t }}</div>
+    </div>
+    <div class="abc-tabs-content">
+      <component class="abc-tabs-content-item" v-for="(c, index) in defaults" :key="index" :is="c" />
+    </div>
   </div>
 </template>
 
@@ -30,5 +33,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
 
+.abc-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
 </style>
