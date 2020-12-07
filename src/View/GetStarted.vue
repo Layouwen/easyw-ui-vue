fd094edc5cfce9248580e5f710df87970c170ec3
liangyouwen <layouwen@gmail.com>
2020-12-07 20:30:47 +0800

    引用github-markdown-css样式，并完善文档文章内容

--- src/View/GetStarted.vue
+++ src/View/GetStarted.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>我是开始页面</div>
+  <article class="markdown-body">
+    <h1>开始使用</h1>
+    <p>如果你第一次使用此框架，请先安装本组件库。</p>
+    <p>接着在你的代码中引入对应的组件</p>
+    <pre><code>import {Button, Tabs, Switch, Dialog} from "wuming-ui"</code></pre>
+    <p>引用后即可在项目中使用该组件库。</p>
+
+    <h2>Vue 单文件组件</h2>
+    <p>代码示例：</p>
+    <pre><code>&lt;template&gt;
+  &lt;div&gt;
+    &lt;Button&gt;按钮组件&lt;/Button&gt;
+  &lt;/div&gt;
+&lt;/template&gt;
+&lt;script&gt;
+  import {Button} from "wuming-ui";
+  export default {
+    components: {Button}
+  }
+&lt;/script&gt;</code></pre>
+  </article>
 </template>
 
 <script lang='ts'>
--- src/View/Install.vue
+++ src/View/Install.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>我是安装页面</div>
+  <article class="markdown-body">
+    <h1>安装</h1>
+    <p>使用npm进行安装</p>
+    <pre><code>npm install wuming-ui</code></pre>
+    <p>使用yarn进行安装</p>
+    <pre><code>yarn add wuming-ui</code></pre>
+    <p>下一篇文章：<a href="#/doc/get-started">开始使用</a></p>
+  </article>
 </template>
 
 <script lang='ts'>
