d2a419b89edbf8797c0e58ad6a835684b776d5b8
liangyouwen <layouwen@gmail.com>
2020-12-07 17:12:40 +0800

    简单完成主页的布局

--- src/View/Home.vue
+++ src/View/Home.vue
@@ -12,15 +12,29 @@
       </div>
     </div>
     <div class="features">
-      <svg class="icon">
-        <use xlink:href="#icon-vue"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-ts"></use>
-      </svg>
-      <svg class="icon">
-        <use xlink:href="#icon-light"></use>
-      </svg>
+      <ul>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-vue"></use>
+          </svg>
+          <h3>基于 Vue 3</h3>
+          <p>框架使用了 Vue 3 Composition API</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-ts"></use>
+          </svg>
+          <h3>基于 TypeScript</h3>
+          <p>源码功能使用 TypeScript 书写</p>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="#icon-light"></use>
+          </svg>
+          <h3>代码简单</h3>
+          <p>多次重构，使用简洁的语法完成</p>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -71,9 +85,42 @@ $border-radius: 4px;
 }
 
 .features {
-  > svg {
-    width: 64px;
-    height: 64px;
+  margin: 64px auto;
+  width: 400px;
+  height: 64px;
+  @media (min-width: 800px) {
+    width: 800px;
+  }
+  @media (min-width: 1200px) {
+    width: 1200px;
+  }
+  > ul {
+    display: flex;
+    flex-wrap: wrap;
+    > li {
+      width: 400px;
+      margin: 16px 0;
+      display: grid;
+      justify-content: start;
+      align-content: space-between;
+      grid-template-areas:
+        "icon title"
+        "icon text";
+      grid-template-columns: 80px auto;
+      grid-template-rows: 1fr auto;
+      > svg {
+        grid-area: icon;
+        width: 64px;
+        height: 64px;
+      }
+      > h3 {
+        grid-area: title;
+        font-size: 28px;
+      }
+      > p {
+        grid-area: text
+      }
+    }
   }
 }
 </style>
