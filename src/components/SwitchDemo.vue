164a97372628b9ebe64fdffb40fda046faf458f5
liangyouwen <layouwen@gmail.com>
2020-12-08 11:32:39 +0800

    抽离Demo组件，并显示其对应标题

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,37 +1,12 @@
 <template>
-  <div>
-    <h1>Switch 组件示例 </h1>
-    <div class="demo">
-      <h2>常规用法</h2>
-      <div class="demo-component">
-        <component :is="Switch1Demo" />
-      </div>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre class="language-html"
-             v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"></pre>
-      </div>
-    </div>
-  </div>
-  <div class="demo">
-    <h2>支持 disabled</h2>
-    <div class="demo-component">
-      <component :is="Switch2Demo" />
-    </div>
-    <div class="demo-actions">
-      <Button>查看代码</Button>
-    </div>
-    <div class="demo-code">
-        <pre class="language-html"
-             v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"></pre>
-    </div>
-  </div>
+  <h1>Switch 组件示例</h1>
+  <Demo :component="Switch1Demo" />
+  <Demo :component="Switch2Demo" />
 </template>
 
 <script lang='ts'>
 import Button from '../lib/Button.vue';
+import Demo from './Demo.vue';
 import Switch1Demo from '../components/Switch1.demo.vue';
 import Switch2Demo from '../components/Switch2.demo.vue';
 import 'prismjs';
@@ -41,7 +16,7 @@ const Prism = (window as any).Prism;
 
 export default {
   name: 'SwitchDemo',
-  components: {Button},
+  components: {Demo, Button},
   setup() {
     return {Switch1Demo, Switch2Demo, Prism};
   },
@@ -49,30 +24,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$border-color: #d9d9d9;
-.demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  > h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, "Courier New", Courier, monospace;
-      margin: 0;
-    }
-  }
-}
 </style>
