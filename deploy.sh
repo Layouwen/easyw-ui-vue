980a5e533054d0257bc709bb9c52e35d620d9753
Avan <layouwen@gmail.com>
2022-12-25 16:55:52 +0800

    build: 部署

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,10 @@ rm -rf dist &&
 yarn build &&
 cd dist &&
 git init &&
+git checkout -b page &&
 git add . &&
 git commit -m 'update' &&
-git branch -M master &&
-git remote add gitee git@gitee.com:layouwen/easyw-ui-vue-website.git &&
-git remote add github git@github.com:Layouwen/easyw-ui-vue-website.git &&
-git push -f -u gitee master &&
-git push -f -u github master &&
+git remote add github git@github.com:Layouwen/easyw-ui-vue.git &&
+git push -f -u github page &&
 cd -s
-echo "http://layouwen.gitee.io/easyw-ui-vue-website/#/"
+echo "https://layouwen.github.io/easyw-ui-vue/#/"
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import fs from 'fs';
 import { baseParse } from '@vue/compiler-core';
 
 export default {
-  base: './',
+  base: '/easyw-ui-vue/',
   assetsDir: 'assets',
   plugins: [md()],
   vueCustomBlockTransforms: {
